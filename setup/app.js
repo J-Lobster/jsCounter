def30c2a5a34b29e32b738466361bb3f804b9687
Albert I Palacios <albertpalacios70@yahoo.com>
2023-12-04 18:26:21 +0000

    Started creating js script for counter increment.

---
+++ setup/app.js
@@ -0,0 +1,8 @@
+// set initial counter value
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns);
